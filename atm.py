cf20bde58fa4cb4864c9d38b94419fb23439ba97
sumit-das-dev <sumitcodes.dev@gmail.com>
2026-04-20 21:33:49 +0530

    add 3-attempt pin retry and card block functionality

--- atm.py
+++ atm.py
@@ -1,13 +1,24 @@
 pin = 4569
 balance = 10000
-user_pin = int(input("Enter Pin = "))
-if user_pin == pin:
-    amount = int(input("Enter Amount = "))
-    if amount <= balance:
-        balance = balance - amount
-        print("Cash given")
-        print("Available balance = " ,balance)
+attempts = 0
+
+while attempts < 3:
+    user_pin = int(input("Enter Pin = "))
+
+    if user_pin == pin:
+        amount = int(input("Enter Amount = "))
+
+        if amount <= balance:
+            balance -= amount
+            print("Cash given")
+            print("Available balance = ", balance)
+        else:
+            print("Insufficient Balance")
+        break   # important
+
     else:
-        print("Insuffient Balance")
-else:
-    print("Invalid Pin")
+        attempts += 1
+        print("Wrong Pin")
+
+if attempts == 3:
+    print("Your Card Has Been Blocked"
